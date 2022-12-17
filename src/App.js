616562f26f2f1b5b601d9c2166102280beb3a48f
ori mamo <mori8056@gmail.com>
2022-12-17 22:44:23 +0200

    amiel homo

--- src/App.js
+++ src/App.js
@@ -1,93 +1,81 @@
 import './App.css';
 import React from "react";
+import Cell from "./Cell";
+
+
 
 class App extends React.Component {
-
     state = {
-        player1: 1,
-        player2: 2,
-        currentPlayer: null,
-        board: [],
-        gameOver: false,
-    };
+        player1:1,
+        player2:2,
+        currentPlayer: 1,
+        board:
+            [
+                ['', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', '']
+            ]
 
-    initBoard() {
-        let board = [];
-        for (let r = 0; r < 6; r++) {
-            let row = [];
-            for (let c = 0; c < 7; c++) { row.push(null) }
-            board.push(row);
-        }
-        this.setState({
-            board,
-            currentPlayer: this.state.player1,
-            gameOver: false,
-        });
     }
 
-    togglePlayer() {
+
+
+    cellClicked = (cell) =>{
+        let currentColor = this.state.board;
+        for (let i = 5; i >= 0; i--) {
+            if (currentColor[i][cell]==='' ){
+                currentColor[i][cell] = this.state.currentPlayer;
+                this.setState({
+                    board: currentColor,
+                    currentPlayer: this.switchPlayer(),
+                })
+                break
+            }
+
+
+
+        }
+
+    }
+
+
+    switchPlayer () {
         return (this.state.currentPlayer === this.state.player1) ? this.state.player2 : this.state.player1;
     }
-
-    play=(c)=> {
-        alert("hi")
-        let board = this.state.board;
-        for (let r = 5; r >= 0; r--) {
-            if (!board[r][c]) {
-                board[r][c] = 1;
-                break;
-            }
-        }
-        this.setState({ board , currentPlayer: this.togglePlayer() });
-    }
-
-    componentWillMount() {
-        this.initBoard();
-    }
-
-    Cell = ( value, columnIndex) => {
-        let color = 'white';
-        if (value === 1) {
-            color = 'red';
-        } else if (value === 2) {
-            color = 'yellow';
-        }
-        return (
-            <td>
-                <div className="cell" onClick={() => {this.play(columnIndex)}}>
-                    <div className={color}></div>
-                </div>
-            </td>
-        );
-    };
-
-    Row = ( row, play ) => {
-        return (
-            <tr>
-                {row.map((cell, i) => <Cell key={i} value={cell} columnIndex={i} play={play}/>)}
-            </tr>
-        );
-    };
-
-
-
     render() {
-        debugger
         return (
-            <div>
-                <h1>Connect 4</h1>
-                <div className="button" onClick={() => {this.initBoard()}}>New Game</div>
+            <div className={"App"}>
+                <label> </label>
                 <table>
-                    <tbody>
-                    {this.state.board.map((row, i) => (<Row key={i} row={row} play={this.play} />))}
-                    </tbody>
+                    {
+                        this.state.board.map((row, rowIndex) => {
+                            return(
+                                <tr>
+                                    {
+                                        row.map((cell, cellIndex) =>{
+                                            return(
+                                                <Cell
+                                                    value = {this.state.board[rowIndex][cellIndex]}
+                                                    cellClicked = {this.cellClicked}
+                                                    row = {rowIndex}
+                                                    cell = {cellIndex}
+                                                />
+                                            )
+                                        })
+                                    }
+                                </tr>
+                            )
+                        })
+                    }
                 </table>
             </div>
         );
     }
 
 }
-
 
 
 export default App;
--- src/Cell.js
+++ src/Cell.js
@@ -1,10 +1,19 @@
-function Cell(props) {
+import './App.css';
+
+
+function Cell(props){
+    let bg = 'white';
+    if (props.value === 1) {
+        bg = 'red';
+    } else if (props.value === 2) {
+        bg = 'yellow';
+    }
     return(
-        <td style = {{backgroundColor: "white"}} onClick={() =>
-            props.cellClicked(props.row, props.cell)}>
-            {props.value}
+        <td style = {{backgroundColor: bg}} onClick={() =>
+            props.cellClicked(props.cell)}>
+
         </td>
     )
 }
 
-export default Cell;
+export default Cell;
